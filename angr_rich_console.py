aadc85539dbfc9a3dbeab868563a97439fc5d9d7
Krietz7 <krietz74@gmail.com>
2025-04-03 17:14:32 +0800

    Implement memory monitoring function

--- angr_rich_console.py
+++ angr_rich_console.py
@@ -4,6 +4,9 @@ import logging
 
 import time
 import threading
+from os import getpid
+from psutil import Process
+
 
 import rich
 from queue import Queue, Empty
@@ -25,16 +28,16 @@ class Timer:
     def _run(self):
         while not self.is_stopped.is_set():
             elapsed = time.time() - self.start_time
-            
+
             hours, rem = divmod(elapsed, 3600)
             minutes, seconds = divmod(rem, 60)
             milliseconds = int((seconds - int(seconds)) * 1000)
-            
+
             time_str = (
                 f"{int(hours):02d}:{int(minutes):02d}:"
                 f"{int(seconds):02d}.{milliseconds:03d}"
             )
-            
+
             self._flush_queue()
             self.queue.put(time_str)
             time.sleep(0.03)  # 30ms Update interval
@@ -51,21 +54,18 @@ class Timer:
         self.thread.join()
 
 
-class SimgrConsoleLive():
+class SimgrCLI():
     def __init__(self):
         self.timer = Timer()
-        
-        self.simgr_text = ""
+        self.simgr_info = SimgrInfo().get_instance()
 
 
-        
+
         self.is_stopped = threading.Event()
-        
+
         self.display_thread = threading.Thread(target=self._run_display)
         self.display_thread.start()
 
-    def set_simgr_text(self, text: str):
-        self.simgr_text = text
 
     def _run_display(self):
         with rich.live.Live(refresh_per_second=CONSOLE_LIVE_REFRESH_TIME_PER_SECOND) as live:
@@ -78,20 +78,24 @@ class SimgrConsoleLive():
                 display_text = rich.text.Text.assemble(
                     ("[Timer] ", "bold cyan"),
                     (time_str, "bold green"),
+                    (" | [Memory usage] ", "bold cyan"),
+                    (self.simgr_info.memory_used, "bold red"),
                     (" | [sim manager] ", "bold cyan"),
-                    (self.simgr_text, "bold yellow")
+                    (self.simgr_info.simgr_text, "bold yellow")
                 )
 
                 live.update(display_text)
+                self.simgr_info.need_update = False
 
     def stop(self):
         """ Stop displaying and clean up resources """
         self.is_stopped.set()
         self.timer.stop()
-        self.display_thread.join()
 
     def __del__(self):
         self.stop()
+        self.simgr_info.clear()
+        self.display_thread.join()
 
 
     @classmethod
@@ -100,19 +104,71 @@ class SimgrConsoleLive():
         if not hasattr(cls, "_instance"):
             cls._instance = cls()
             is_new_instance = True
-            
 
         # disable angr logger
         logging.getLogger("angr").propagate = False
+        logging.getLogger("claripy").propagate = False
         return cls._instance, is_new_instance
 
     @classmethod
     def del_instance(cls):
+        while(cls._instance.simgr_info.need_update == True):
+            time.sleep(0.1)
+
         if hasattr(cls, "_instance"):
             cls._instance.__del__()
 
         # recover angr logger
         logging.getLogger("angr").propagate = True
+        logging.getLogger("claripy").propagate = True
+
+
+
+
+
+
+class SimgrInfo():
+    def __init__(self):
+        self.simgr_text = ""
+        self.memory_used = ""
+
+
+
+
+
+        self.need_update = False
+
+    def get_info_from_simgr(self, simgr: angr.sim_manager.SimulationManager):
+        self.simgr_text = str(simgr)
+
+        self.memory_used = self._get_memory_usage()
+
+
+
+
+
+        self.need_update = True
+
+    def _get_memory_usage(self):
+        process = Process(getpid())
+        memory_usage = process.memory_info().vms / float(2 ** 30)
+        return '{:.4f}'.format(memory_usage) + " GB"
+        
+
+
+
+    def clear(self):
+        self.simgr_text = ""
+        self.need_update = False
+
+    @classmethod
+    def get_instance(cls):
+        if not hasattr(cls, "_instance"):
+            cls._instance = cls()
+        return cls._instance
+
+
+
 
 
 
@@ -120,39 +176,73 @@ class SimgrConsoleLive():
 
 @aspectlib.Aspect
 def simgr_step(*args, **kwargs):
-    simg_logger, is_instance_creater = SimgrConsoleLive.get_instance()
+    simgr_cli, is_instance_creater = SimgrCLI.get_instance()
+    try:
+        result = yield aspectlib.Proceed
+        if is_instance_creater:
+            simgr_cli.del_instance()
+        yield aspectlib.Return(result)
+    except:
+        if is_instance_creater:
+            simgr_cli.del_instance()
+        raise
 
-    result = yield aspectlib.Proceed
-    if is_instance_creater:
-        simg_logger.del_instance()
-    yield aspectlib.Return(result)
+
 
 
 
 @aspectlib.Aspect
 def simgr_run(*args, **kwargs):
-    simg_logger, is_instance_creater = SimgrConsoleLive.get_instance()
-
-    result = yield aspectlib.Proceed
-    if is_instance_creater:
-        simg_logger.del_instance()
-    yield aspectlib.Return(result)
+    simgr_cli, is_instance_creater = SimgrCLI.get_instance()
+    try:
+        result = yield aspectlib.Proceed
+        if is_instance_creater:
+            simgr_cli.del_instance()
+        yield aspectlib.Return(result)
+    except:
+        if is_instance_creater:
+            simgr_cli.del_instance()
+        raise
 
 
 
 @aspectlib.Aspect
 def simgr_step_state(*args, **kwargs):
-    simg_logger, is_instance_creater = SimgrConsoleLive.get_instance()
-    simg_logger.set_simgr_text(str(args[0]))
+    simgr_cli, is_instance_creater = SimgrCLI.get_instance()
+    try:
+        simgr_info = SimgrInfo.get_instance()
+        simgr_info.get_info_from_simgr(args[0])
 
-    result = yield aspectlib.Proceed
-    if is_instance_creater:
-        simg_logger.del_instance()
-    yield aspectlib.Return(result)
+        result = yield aspectlib.Proceed
+        if is_instance_creater:
+            simgr_cli.del_instance()
+        yield aspectlib.Return(result)
+    except:
+        if is_instance_creater:
+            simgr_cli.del_instance()
+        raise
 
 
 
+# weave_context_step = aspectlib.weave(angr.sim_manager.SimulationManager.step, simgr_step)
+# weave_context_run = aspectlib.weave(angr.sim_manager.SimulationManager.run, simgr_run)
+# weave_context_step_state = aspectlib.weave(angr.sim_manager.SimulationManager.step_state, simgr_step_state)
 
-weave_context_step = aspectlib.weave(angr.sim_manager.SimulationManager.step, simgr_step)
-weave_context_run = aspectlib.weave(angr.sim_manager.SimulationManager.run, simgr_run)
-weave_context_step_state = aspectlib.weave(angr.sim_manager.SimulationManager.step_state, simgr_step_state)
+class Console_Rich:
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        self.weave_context_step_state = aspectlib.weave(angr.sim_manager.SimulationManager.step_state, simgr_step_state)
+        self.weave_context_step = aspectlib.weave(angr.sim_manager.SimulationManager.step, simgr_step)
+        self.weave_context_run = aspectlib.weave(angr.sim_manager.SimulationManager.run, simgr_run)
+
+        self.weave_context_step_state.__enter__()
+        self.weave_context_step.__enter__()
+        self.weave_context_run.__enter__()
+        return self
+
+    def __exit__(self, *args):
+        self.weave_context_step_state.__exit__(*args)
+        self.weave_context_step.__exit__(*args)
+        self.weave_context_run.__exit__(*args)
